810f9f1fca9760faa30d7c8b2f9f81802219a4e1
Joseph Chretien-Golden <joechre@gmail.com>
2019-08-26 13:22:00 -0500

    made new prep time field searchable

--- src/Components/Container/YourNotes.js
+++ src/Components/Container/YourNotes.js
@@ -27,14 +27,14 @@ export class YourNotes extends Component {
         // Need to figure out a way to get layout2 out of this render function. Maybe in state.
         // onClick={() => this.props.history.push(`notes/note/${note._id}`)}
         let layout2 = this.props.notes.filter(
-            note => (note.title + " " + note.textBody + " " + note.tags).toLowerCase().includes(`${this.props.currentSearch.sidebarSearch}`.toLowerCase())).map(
+            note => (note.title + " " + note.textBody + " " + note.tags + " " + note.recipetime).toLowerCase().includes(`${this.props.currentSearch.sidebarSearch}`.toLowerCase())).map(
                 (note, index) => {return {i: note._id ? note._id.toString() : '', x: (index % 6) * 3, y: Infinity, w: 3, h: 6, minH: 6, minW: 2}});
 
         return (
             <article className="your-notes">
                 <GridLayout className="layout" layout={layout2} cols={18} rowHeight={30} width={1550} margin={[15, 15]} compactType="vertical">
                 {this.props.notes.filter(
-                    note => (note.title + " " + note.textBody + " " + note.tags).toLowerCase().includes(`${this.props.currentSearch.sidebarSearch}`.toLowerCase())).map(
+                    note => (note.title + " " + note.textBody + " " + note.tags + " " + note.recipetime).toLowerCase().includes(`${this.props.currentSearch.sidebarSearch}`.toLowerCase())).map(
                         (note, id) => (
                             <div id={GridLayout.state} className="note" key={note._id.toString()} 
                             onDoubleClick={() => this.props.history.push(`note/${note._id}`)}

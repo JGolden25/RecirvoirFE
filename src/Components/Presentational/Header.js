1958e4ad6eb3ee4fe30f432f8b72f9a5d4026d4e
Joseph Chretien-Golden <joechre@gmail.com>
2019-08-26 23:54:27 -0500

    new fields ingredients and steps added

--- src/Components/Presentational/Header.js
+++ src/Components/Presentational/Header.js
@@ -18,6 +18,8 @@ export const Header = props => {
                 <input name="title" placeholder="Dish" onChange={e => props.handleInput(e)}/>
                 <textarea name="textBody" onClick={displayCreateNoteForm} onFocus={displayCreateNoteForm} onChange={e => props.handleInput(e)} placeholder="Recipe Description..."></textarea>
                 <textarea name="recipetime" onClick={displayCreateNoteForm} onFocus={displayCreateNoteForm} onChange={e => props.handleInput(e)} placeholder="Preparation Time (in minutes)..."></textarea>
+                <textarea name="ingredients" onClick={displayCreateNoteForm} onFocus={displayCreateNoteForm} onChange={e => props.handleInput(e)} placeholder="Ingredients"></textarea>
+                <textarea name="steps" onClick={displayCreateNoteForm} onFocus={displayCreateNoteForm} onChange={e => props.handleInput(e)} placeholder="Steps"></textarea>
                 <div className="buttons">
                     <div className="button create" onClick={(e) => {props.assembleNote(props.note); hideCreateNoteForm(e)}}>Add Recipe</div> 
                     <div className="button close" onClick={hideCreateNoteForm}>Close</div>
